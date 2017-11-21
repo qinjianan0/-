555053943ffacdb9bb557e25e6e42b42cd964504
Foxconn 38 <foxconn@FoxconndeiMac.local>
2017-11-21 09:21:25 +0800

    Initial Commit

---
+++ 考勤/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  考勤
+//
+//  Created by Foxconn 38 on 2017/11/21.
+//  Copyright © 2017年 Foxconn 38. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
